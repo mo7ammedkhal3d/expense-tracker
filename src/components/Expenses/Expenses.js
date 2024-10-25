685d41b5c05dd8fec375f77e800da0fe4abe1edc
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-10-25 13:54:11 +0300

    new changes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -64,7 +64,7 @@ const Expenses = (props) =>{
 return (
     <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+        {props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
     </Card>
 );
 }
